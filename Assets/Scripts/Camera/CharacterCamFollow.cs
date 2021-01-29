ad210ef51dce0c65647f1c92c88461135288d5b8
Shark74930 <64776697+Shark74930@users.noreply.github.com>
2021-01-29 20:29:02 +0100

    Camera

---
+++ Assets/Scripts/Camera/CharacterCamFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamFollow : MonoBehaviour
+{
+    #region Serialized
+    [SerializeField] private Transform target;
+    [SerializeField] private float smooth;
+    [Range(0.01f, 1.0f)]
+    #endregion Serialized
+
+    #region Property
+    private Vector3 offsetCamera;
+    #endregion Property
+
+    #region Methods
+    void Start()
+    {
+        offsetCamera = transform.position - target.position;
+    }
+
+    void Update()
+    {
+        Vector3 cameraPosition = target.position + offsetCamera;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, cameraPosition, smooth);
+        transform.position = smoothPosition;
+        transform.LookAt(target);
+    }
+    #endregion Methods
+}
