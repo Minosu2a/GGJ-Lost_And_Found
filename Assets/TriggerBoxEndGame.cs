020868d7ae905fba3d05abdae979298048fdd995
Minosu2a <64777323+Minosu2a@users.noreply.github.com>
2021-01-31 15:30:56 +0100

    AAAAAAAAAAAAAAAAAAAAH

---
+++ Assets/TriggerBoxEndGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBoxEndGame : MonoBehaviour
+{
+
+    [SerializeField] CharacterStateController _character = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Carillon")
+        {
+            _character.ChangeState(ECharacterState.INTERACTION);
+        }
+    }
+}
